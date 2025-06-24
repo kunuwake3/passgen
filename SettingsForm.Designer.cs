fee2615e80166bf604c22f624ec7b5ec622dc421
kunuwake3 <kunuwake3@gmail.com>
2025-06-24 20:18:20 +0800

    Update SettingsForm.Designer.cs

--- SettingsForm.Designer.cs
+++ SettingsForm.Designer.cs
@@ -1,104 +1,53 @@
-namespace TrayPassGen
+namespace TrayPassGen.Properties
 {
-    partial class SettingsForm
+    [global::System.Runtime.CompilerServices.CompilerGenerated()]
+    [global::System.CodeDom.Compiler.GeneratedCode("SettingsSingleFileGenerator", "1.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
     {
-        private System.ComponentModel.IContainer components = null;
-        private System.Windows.Forms.CheckBox cbLower;
-        private System.Windows.Forms.CheckBox cbUpper;
-        private System.Windows.Forms.CheckBox cbDigits;
-        private System.Windows.Forms.CheckBox cbSpecial;
-        private System.Windows.Forms.CheckBox cbSafeSymbols;
-        private System.Windows.Forms.NumericUpDown nudLength;
-        private System.Windows.Forms.TextBox tbPrefix;
-        private System.Windows.Forms.Button btnSave;
-        private System.Windows.Forms.Label labelLength;
-        private System.Windows.Forms.Label labelPrefix;
+        private static Settings defaultInstance = ((Settings)(Synchronized(new Settings())));
 
-        protected override void Dispose(bool disposing)
+        public static Settings Default => defaultInstance;
+
+        public bool IncludeLowercase
         {
-            if (disposing && (components != null))
-                components.Dispose();
-            base.Dispose(disposing);
+            get => ((bool)this["IncludeLowercase"]);
+            set => this["IncludeLowercase"] = value;
         }
 
-        private void InitializeComponent()
+        public bool IncludeUppercase
         {
-            cbLower = new System.Windows.Forms.CheckBox();
-            cbUpper = new System.Windows.Forms.CheckBox();
-            cbDigits = new System.Windows.Forms.CheckBox();
-            cbSpecial = new System.Windows.Forms.CheckBox();
-            cbSafeSymbols = new System.Windows.Forms.CheckBox();
-            nudLength = new System.Windows.Forms.NumericUpDown();
-            tbPrefix = new System.Windows.Forms.TextBox();
-            btnSave = new System.Windows.Forms.Button();
-            labelLength = new System.Windows.Forms.Label();
-            labelPrefix = new System.Windows.Forms.Label();
+            get => ((bool)this["IncludeUppercase"]);
+            set => this["IncludeUppercase"] = value;
+        }
 
-            ((System.ComponentModel.ISupportInitialize)(nudLength)).BeginInit();
-            SuspendLayout();
+        public bool IncludeDigits
+        {
+            get => ((bool)this["IncludeDigits"]);
+            set => this["IncludeDigits"] = value;
+        }
 
-            // Чекбоксы
-            cbLower.Text = "Строчные буквы (a-z)";
-            cbLower.Location = new System.Drawing.Point(12, 12);
-            cbLower.AutoSize = true;
+        public bool IncludeSpecialChars
+        {
+            get => ((bool)this["IncludeSpecialChars"]);
+            set => this["IncludeSpecialChars"] = value;
+        }
 
-            cbUpper.Text = "Заглавные буквы (A-Z)";
-            cbUpper.Location = new System.Drawing.Point(12, 35);
-            cbUpper.AutoSize = true;
+        public bool UseSafeSymbols
+        {
+            get => ((bool)this["UseSafeSymbols"]);
+            set => this["UseSafeSymbols"] = value;
+        }
 
-            cbDigits.Text = "Цифры (0-9)";
-            cbDigits.Location = new System.Drawing.Point(12, 58);
-            cbDigits.AutoSize = true;
+        public int PasswordLength
+        {
+            get => ((int)this["PasswordLength"]);
+            set => this["PasswordLength"] = value;
+        }
 
-            cbSpecial.Text = "Спецсимволы (!@#$)";
-            cbSpecial.Location = new System.Drawing.Point(12, 81);
-            cbSpecial.AutoSize = true;
-
-            cbSafeSymbols.Text = "Только безопасные спецсимволы (_-+=!@#.)";
-            cbSafeSymbols.Location = new System.Drawing.Point(12, 104);
-            cbSafeSymbols.AutoSize = true;
-
-            // Длина
-            labelLength.Text = "Длина пароля:";
-            labelLength.Location = new System.Drawing.Point(12, 135);
-            labelLength.AutoSize = true;
-
-            nudLength.Location = new System.Drawing.Point(120, 133);
-            nudLength.Minimum = 4;
-            nudLength.Maximum = 128;
-            nudLength.Value = 16;
-
-            // Префикс
-            labelPrefix.Text = "Префикс:";
-            labelPrefix.Location = new System.Drawing.Point(12, 162);
-            labelPrefix.AutoSize = true;
-
-            tbPrefix.Location = new System.Drawing.Point(120, 159);
-            tbPrefix.Width = 140;
-
-            // Сохранить
-            btnSave.Text = "Сохранить";
-            btnSave.Location = new System.Drawing.Point(90, 195);
-            btnSave.Click += new System.EventHandler(btnSave_Click);
-
-            // Форма
-            ClientSize = new System.Drawing.Size(300, 240);
-            Controls.AddRange(new Control[]
-            {
-                cbLower, cbUpper, cbDigits, cbSpecial, cbSafeSymbols,
-                labelLength, nudLength, labelPrefix, tbPrefix,
-                btnSave
-            });
-
-            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            MaximizeBox = false;
-            MinimizeBox = false;
-            Text = "Настройки генератора";
-            StartPosition = FormStartPosition.CenterScreen;
-
-            ((System.ComponentModel.ISupportInitialize)(nudLength)).EndInit();
-            ResumeLayout(false);
-            PerformLayout();
+        public string StaticPrefix
+        {
+            get => ((string)this["StaticPrefix"]);
+            set => this["StaticPrefix"] = value;
         }
     }
 }
